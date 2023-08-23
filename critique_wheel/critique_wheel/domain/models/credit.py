2956c7854ce9b6c676e73047bdcdf6dd3f67e003
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-23 11:59:22 +0200

    Add credit transaction validation
    Critique given must contian work_id and critique_id
    Work submitted must only contain work_id

--- critique_wheel/critique_wheel/domain/models/credit.py
+++ critique_wheel/critique_wheel/domain/models/credit.py
@@ -14,14 +14,16 @@ class CreditTransaction:
     def __init__(
         self,
         member_id,
-        work_id,
         amount,
+        work_id=None,
+        critique_id=None,
         transaction_type=TransactionType.CRITIQUE_GIVEN,
         transaction_id=None,
         date=None,
     ):
         self.id = transaction_id or uuid4()
         self.member_id = member_id
+        self.critique_id = critique_id
         self.work_id = work_id
         self.amount = amount
         self.date_of_transaction = date or datetime.now()
@@ -34,12 +36,18 @@ class CreditTransaction:
         amount,
         transaction_type=TransactionType.CRITIQUE_GIVEN,
         work_id=None,
+        critique_id=None,
         transaction_id=None,
         date=None,
     ):
+        if transaction_type == TransactionType.WORK_SUBMITTED and (work_id is None or critique_id is not None):
+            raise ValueError("Work submission must only have an associated work_id.")
+        if transaction_type == TransactionType.CRITIQUE_GIVEN and (critique_id is None or work_id is None):
+            raise ValueError("Critique submission must have an associated critique_id and work_id.")
         return cls(
             member_id=member_id,
             work_id=work_id,
+            critique_id=critique_id,
             amount=amount,
             transaction_type=transaction_type,
             transaction_id=transaction_id,
--- critique_wheel/tests/unit/test_credit.py
+++ critique_wheel/tests/unit/test_credit.py
@@ -5,35 +5,149 @@ import pytest
 from critique_wheel.domain.models.credit import CreditTransaction, TransactionType
 
 
-def test_can_generate_credit_transation():
-    ct = CreditTransaction(
-        member_id=42,
-        work_id=45,
-        amount=3,
-        transaction_id=23,
-        date=datetime.now(),
-    )
-    assert ct.id == 23
-    assert ct.member_id == 42
-    assert ct.work_id == 45
-    assert ct.amount == 3
-    assert ct.transaction_type == TransactionType.CRITIQUE_GIVEN
-    assert ct.date_of_transaction.date() == datetime.now().date()
+@pytest.mark.current
+class TestTransactionType:
+    def test_can_generate_credit_transation(self):
+        ct = CreditTransaction(
+            member_id=42,
+            work_id=45,
+            critique_id=46,
+            amount=3,
+            transaction_id=23,
+            date=datetime.now(),
+        )
+        assert ct.id == 23
+        assert ct.member_id == 42
+        assert ct.critique_id == 46
+        assert ct.work_id == 45
+        assert ct.amount == 3
+        assert ct.transaction_type == TransactionType.CRITIQUE_GIVEN
+        assert ct.date_of_transaction.date() == datetime.now().date()
 
-def test_create_transaction():
-    ct = CreditTransaction.create(
-        member_id=42,
-        work_id=45,
-        amount=3,
-        transaction_id=23,
-        transaction_type=TransactionType.WORK_SUBMITTED,
-        date=datetime.now(),
-    )
-    assert ct.id == 23
-    assert ct.member_id == 42
-    assert ct.work_id == 45
-    assert ct.amount == 3
-    assert ct.transaction_type == TransactionType.WORK_SUBMITTED
-    assert ct.date_of_transaction.date() == datetime.now().date()
+    def test_create_transaction(self):
+        ct = CreditTransaction.create(
+            member_id=42,
+            work_id=45,
+            critique_id=None,
+            amount=3,
+            transaction_id=23,
+            transaction_type=TransactionType.WORK_SUBMITTED,
+            date=datetime.now(),
+        )
+        assert ct.id == 23
+        assert ct.member_id == 42
+        assert ct.critique_id == None
+        assert ct.work_id == 45
+        assert ct.amount == 3
+        assert ct.transaction_type == TransactionType.WORK_SUBMITTED
+        assert ct.date_of_transaction.date() == datetime.now().date()
 
+    def test_critique_given_credit_transaction_with_critique_id_and_work_id(self):
+        member_id = 45
+        amount = 3
+        work_id = 45
+        transaction_type = TransactionType.CRITIQUE_GIVEN
+        critique_id = 46
 
+        ct = CreditTransaction.create(
+            member_id=member_id,
+            amount=amount,
+            transaction_type=transaction_type,
+            critique_id=critique_id,
+            work_id=work_id,
+        )
+        assert ct.critique_id == critique_id
+        assert ct.work_id == work_id
+        assert ct.transaction_type == TransactionType.CRITIQUE_GIVEN
+
+    def test_critique_given_credit_transaction_without_work_id(self):
+        member_id = 43
+        amount = 3
+        work_id = None  # deliberately set to None
+        critique_id = 46
+        transaction_type = TransactionType.CRITIQUE_GIVEN
+
+        with pytest.raises(
+            ValueError,
+            match="Critique submission must have an associated critique_id and work_id.",
+        ):
+            CreditTransaction.create(
+                member_id=member_id,
+                amount=amount,
+                transaction_type=transaction_type,
+                work_id=work_id,
+                critique_id=critique_id,
+                transaction_id=None,
+                date=None,
+            )
+
+    def test_critique_given_credit_transaction_without_critique_id(self):
+        member_id = 43
+        amount = 3
+        work_id = 45
+        critique_id = None  # deliberately set to None
+        transaction_type = TransactionType.CRITIQUE_GIVEN
+
+        with pytest.raises(
+            ValueError,
+            match="Critique submission must have an associated critique_id",
+        ):
+            CreditTransaction.create(
+                member_id=member_id,
+                amount=amount,
+                transaction_type=transaction_type,
+                work_id=work_id,
+                critique_id=critique_id,
+                transaction_id=None,
+                date=None,
+            )
+
+    def test_work_submitted_credit_transaction_with_work_id_and_with_critique_id(self):
+        member_id = 45
+        amount = 3
+        transaction_type = TransactionType.CRITIQUE_GIVEN
+        work_id = 45
+        critique_id = 46
+
+        ct = CreditTransaction.create(
+            member_id=member_id,
+            amount=amount,
+            transaction_type=transaction_type,
+            work_id=work_id,
+            critique_id=critique_id,
+        )
+        assert ct.work_id == 45
+
+    def test_work_submission_credit_transaction_without_work_id(self):
+        member_id = 43
+        amount = 3
+        transaction_type = TransactionType.WORK_SUBMITTED
+
+        with pytest.raises(ValueError, match="Work submission must only have an associated work_id."):
+            CreditTransaction.create(
+                member_id=member_id,
+                amount=amount,
+                transaction_type=transaction_type,
+                work_id=None,
+                critique_id=None,
+                transaction_id=None,
+                date=None,
+            )
+
+    def test_work_submission_credit_transaction_with_work_id_and_critique_id(self):
+        member_id = 43
+        amount = 3
+        work_id = 45
+        critique_id = 46
+        transaction_type = TransactionType.WORK_SUBMITTED
+
+        with pytest.raises(ValueError, match="Work submission must only have an associated work_id"):
+            CreditTransaction.create(
+                member_id=member_id,
+                amount=amount,
+                transaction_type=transaction_type,
+                work_id=work_id,
+                critique_id=critique_id,
+                transaction_id=None,
+                date=None,
+            )
