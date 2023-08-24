d600f9d7326d81915e06f7e086f5099d75a5def8
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-25 00:51:47 +0200

    Add Credit Repo pattern

---
+++ critique_wheel/critique_wheel/domain/models/credit_repository.py
@@ -0,0 +1,18 @@
+import abc
+from typing import List, Optional
+from uuid import UUID
+from critique_wheel.domain.models.credit import CreditManager
+
+class AbstractCreditRepository(abc.ABC):
+
+    @abc.abstractmethod
+    def add(self, credit: CreditManager) -> None:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get(self, credit_id: UUID) -> Optional[CreditManager]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def list(self) -> List[CreditManager]:
+        raise NotImplementedError
