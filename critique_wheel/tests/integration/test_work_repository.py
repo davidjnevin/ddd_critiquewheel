2771bc8ad89eb746e8beb0dab8f2ae8aeda2de5c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-24 18:14:00 +0200

    Refactor to use db_utils to format uuid
    Based on feature flag
    SQLite or Postgres db

--- critique_wheel/tests/integration/test_work_repository.py
+++ critique_wheel/tests/integration/test_work_repository.py
@@ -1,6 +1,10 @@
+import uuid
+
 from sqlalchemy import text
 
 from critique_wheel.adapters.sqlalchemy import work_repository
+from critique_wheel.infrastructure.utils.db_utils import format_uuid_for_db
+
 
 
 def test_repository_can_save_a_work(session, valid_work):
@@ -9,8 +13,17 @@ def test_repository_can_save_a_work(session, valid_work):
     repo.add(work)
     session.commit()
 
-    rows = list(session.execute(text('SELECT title, content, age_restriction, genre FROM "works"')))
-    assert rows == [(work.title, work.content, work.age_restriction.value.upper(), work.genre.value.upper())]
+    rows = list(session.execute(text('SELECT id, title, content, age_restriction, genre, member_id FROM "works"')))
+    assert rows == [
+        (
+            format_uuid_for_db(work.id),
+            work.title,
+            work.content,
+            work.age_restriction.value,
+            work.genre.value,
+            format_uuid_for_db(work.member_id),
+        )
+    ]
 
 
 def test_repository_can_get_a_work(session, valid_work):
@@ -20,6 +33,17 @@ def test_repository_can_get_a_work(session, valid_work):
     session.commit()
 
     id_to_get = work.id
-    stmt = text('SELECT title, content, age_restriction, genre FROM "works" WHERE id=:id').bindparams(id=id_to_get)
+    stmt = text('SELECT id, title, content, age_restriction, genre, member_id FROM "works" WHERE id=:id').bindparams(
+        id=id_to_get
+    )
     rows = session.execute(stmt).fetchall()
-    assert rows == [(work.title, work.content, work.age_restriction.value.upper(), work.genre.value.upper())]
+    assert rows == [
+        (
+            format_uuid_for_db(work.id),
+            work.title,
+            work.content,
+            work.age_restriction.value,
+            work.genre.value,
+            format_uuid_for_db(work.member_id),
+        )
+    ]
