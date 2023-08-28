83e0528f91877625b120fc62c926251355a04066
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-28 15:08:31 +0200

    Add tests to validate member - critique relationship

--- critique_wheel/tests/integration/test_IAM_repository.py
+++ critique_wheel/tests/integration/test_IAM_repository.py
@@ -1,46 +1,34 @@
-from sqlalchemy import text
+import pytest
+from sqlalchemy import String, Uuid, text, bindparam
 
 from critique_wheel.adapters.sqlalchemy import iam_repository
-from critique_wheel.infrastructure.utils.db_utils import format_uuid_for_db
+from critique_wheel.domain.models.IAM import MemberStatus
+from critique_wheel.domain.models.work import Work
+from critique_wheel.domain.models.critique import Critique
+from critique_wheel.infrastructure.utils import db_utils
 
 
-def test_repository_can_save_a_member(session, valid_member):
-    member = valid_member
+@pytest.mark.current
+def test_repository_can_save_a_basic_member(session, active_valid_member, valid_work, valid_critique):
+    member = active_valid_member
     repo = iam_repository.SqlAlchemyMemberRepository(session)
+    assert member.works == []
+    assert member.critiques == []
     repo.add(member)
+    member.add_work(valid_work)
+    member.add_critique(valid_critique)
     session.commit()
 
-    rows = list(session.execute(text(
-        'SELECT id, username, email, password, member_type, status FROM "members"'
-    )))
-    assert rows == [
-        (
-            format_uuid_for_db(member.id),
-            member.username,
-            member.email,
-            member.password,
-            member.member_type.value,
-            member.status.value,
+    rows = list(
+        session.execute(
+            text(
+                'SELECT id, username, email, password, member_type, status FROM "members"'
+            )
         )
-    ]
-
-
-def test_repository_can_get_a_member_by_id(session, valid_member):
-    member = valid_member
-    repo = iam_repository.SqlAlchemyMemberRepository(session)
-    repo.add(member)
-    session.commit()
-
-    id_to_get = member.id
-    stmt = text(
-        'SELECT id, username, email, password, member_type, status FROM "members" WHERE id=:id'
-    ).bindparams(
-        id=id_to_get
     )
-    rows = session.execute(stmt).fetchall()
     assert rows == [
         (
-            format_uuid_for_db(member.id),
+            db_utils.format_uuid_for_db(member.id),
             member.username,
             member.email,
             member.password,
@@ -48,4 +36,43 @@ def test_repository_can_get_a_member_by_id(session, valid_member):
             member.status.value,
         )
     ]
+
+
+def test_repository_can_get_a_member_by_id(session, valid_member, valid_work, valid_critique):
+    member = valid_member
+    valid_member.status = MemberStatus.ACTIVE
+    repo = iam_repository.SqlAlchemyMemberRepository(session)
+    repo.add(member)
+    valid_work.member_id = member.id
+    valid_critique.member_id = member.id
+    member.add_work(valid_work)
+    member.add_critique(valid_critique)
+    assert len(member.works) == 1
+    assert len(member.critiques) == 1
+    session.commit()
+
+    stmt = (
+        text(
+            'SELECT * FROM "members" WHERE id=:member_id'
+            # 'SELECT id, username, email, password, member_type, status, works, critiques FROM "members" WHERE id=:id'
+        )).bindparams(
+            bindparam("member_id", type_=Uuid),
+        ).bindparams(
+            member_id=valid_member.id,
+        )
+    rows = session.execute(stmt).fetchall()
+    assert len(rows) == 1
+
+
+    retrieved_works = session.query(Work).filter_by(member_id=valid_member.id).all()
+    assert len(retrieved_works) == 1
+    assert retrieved_works[0].title == valid_work.title
+
+    retrieved_critiques = session.query(Critique).filter_by(member_id=valid_member.id).all()
+    assert len(retrieved_critiques) == 1
+    assert retrieved_critiques[0].content_about == valid_critique.content_about
+
+    assert retrieved_works[0].member_id == valid_member.id
+    assert retrieved_critiques[0].member_id == valid_member.id
+
 
