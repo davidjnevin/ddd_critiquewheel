144dbff13936f1e0f35a8925549e764c42aa30e4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-08-30 18:59:47 +0200

    Setup docker and postgres with FastApi

--- critique_wheel/tests/conftest.py
+++ critique_wheel/tests/conftest.py
@@ -2,8 +2,8 @@ from uuid import uuid4
 
 import pytest
 from sqlalchemy import create_engine
-from sqlalchemy.pool import StaticPool
 from sqlalchemy.orm import clear_mappers, sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from critique_wheel.adapters.orm import mapper_registry, start_mappers
 from critique_wheel.domain.models.credit import CreditManager, TransactionType
@@ -11,6 +11,7 @@ from critique_wheel.domain.models.critique import Critique
 from critique_wheel.domain.models.IAM import Member, MemberRole, MemberStatus
 from critique_wheel.domain.models.rating import Rating
 from critique_wheel.domain.models.work import Work, WorkAgeRestriction, WorkGenre
+from critique_wheel.infrastructure.config import config
 
 
 @pytest.fixture
@@ -93,6 +94,7 @@ def valid_work():
         critiques=None,
     )
 
+
 @pytest.fixture
 def another_valid_work():
     return Work.create(
@@ -159,6 +161,22 @@ def in_memory_db():
 def session(in_memory_db):
     start_mappers()
     session = sessionmaker(bind=in_memory_db)()
+    yield session
+    clear_mappers()
+    # session.close()
+
+
+@pytest.fixture
+def postgres_db():
+    engine = create_engine(config.get_postgres_uri(), echo=True)
+    mapper_registry.metadata.create_all(engine)
+    return engine
+
+
+@pytest.fixture
+def postgres_session(postgres_db):
+    start_mappers()
+    session = sessionmaker(bind=postgres_db)()
     yield session
     clear_mappers()
     # session.close()
