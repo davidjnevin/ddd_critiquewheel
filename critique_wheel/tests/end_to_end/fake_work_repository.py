99345dbf6227f26be0576bd6b8ff357bd5c52e4e
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-09-02 20:44:21 +0200

    Return none for not found
    And add self.commit()

--- critique_wheel/tests/end_to_end/fake_work_repository.py
+++ critique_wheel/tests/end_to_end/fake_work_repository.py
@@ -1,12 +1,13 @@
 from sqlalchemy import UUID
 from typing import Optional
-from critique_wheel.domain.models.work import Work, WorkNotFoundException
+from critique_wheel.domain.models.work import Work
 from critique_wheel.domain.models.work_repository import AbstractWorkRepository
 
 
 class FakeWorkRepository(AbstractWorkRepository):
     def __init__(self, works: list[Work]):
         self._works = set(works)
+        self.committed = False
 
     def add(self, work: Work) -> None:
         self._works.add(work)
@@ -15,13 +16,16 @@ class FakeWorkRepository(AbstractWorkRepository):
         try:
             return next(w for w in self._works if w.id == work_id)
         except StopIteration:
-            raise WorkNotFoundException("Not found")
+            return None
 
     def get_work_by_member_id(self, member_id: UUID) -> Optional[Work]:
         try:
             return next(w for w in self._works if w.member_id == member_id)
         except StopIteration:
-            raise WorkNotFoundException("Not found")
+            return None
 
     def list(self) -> list[Work]:
         return list(self._works)
+
+    def commit(self) -> None:
+        self.committed = True
