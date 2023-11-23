c5a6036593bf46ae5823948a75c2b0367cde3353
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-11-23 12:45:48 +0100

    Add value objects

---
+++ critique_wheel/critique_wheel/works/value_objects.py
@@ -0,0 +1,101 @@
+import uuid
+from dataclasses import dataclass, field
+from enum import Enum
+
+
+class BaseWorkDomainError(Exception):
+    pass
+
+
+class MissingEntryError(BaseWorkDomainError):
+    pass
+
+
+class WorkStatus(str, Enum):
+    PENDING_REVIEW = "PENDING REVIEW"
+    ACTIVE = "ACTIVE"
+    REJECTED = "REJECTED"
+    ARCHIVED = "ARCHIVED"
+    MARKED_FOR_DELETION = "MARKED FOR DELETION"
+
+
+class WorkAgeRestriction(str, Enum):
+    NONE = "NONE"
+    TEEN = "TEEN"
+    ADULT = "ADULT"
+
+
+class WorkGenre(str, Enum):
+    BIOGRAPHY = "BIOGRAPHY"
+    CHICKLIT = "CHICKLIT"
+    CHILDREN = "CHILDREN"
+    COMEDY = "COMEDY"
+    CRIME = "CRIME"
+    DRAMA = "DRAMA"
+    FANTASY = "FANTASY"
+    HISTORICALFICTION = "HISTORICAL FICTION"
+    HORROR = "HORROR"
+    LITERARY = "LITERARY"
+    MYSTERY = "MYSTERY"
+    NEWADULT = "NEW ADULT"
+    PARANORMAL = "PARANORMAL"
+    ROMANCE = "ROMANCE"
+    SCIENCEFICTION = "SCIENCE FICTION"
+    SPECULATIVE = "SPECULATIVE"
+    SUSPENSE = "SUSPENSE"
+    THRILLER = "THRILLER"
+    UNDECIDED = "UNDECIDED"
+    URBANFANTASY = "URBAN FANTASY"
+    WOMENSLIT = "WOMENS LIT"
+    YOUNGADULT = "YOUNG ADULT"
+    OTHER = "OTHER"
+
+
+@dataclass(frozen=True)
+class WorkId:
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
+
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def hex(self):
+        return self.id.hex
+
+    def get_uuid(self):
+        return str(self.id)
+
+
+@dataclass(frozen=True)
+class Title:
+    value: str
+
+    def __post_init__(self):
+        if len(self.value) > 100:
+            raise ValueError("Title must be under 100 characters.")
+        if not self.value:
+            raise MissingEntryError("Title cannot be empty.")
+
+    def __str__(self):
+        return str(self.value)
+
+    def __len__(self):
+        return len(self.value)
+
+
+@dataclass(frozen=True)
+class Content:
+    value: str
+
+    def __post_init__(self):
+        char_limit = 8500
+        if len(self.value) > char_limit:
+            raise ValueError(f"Work text must be under {char_limit} characters.")
+        if not self.value:
+            raise MissingEntryError("Content cannot be empty.")
+
+    def __str__(self):
+        return str(self.value)
+
+    def __len__(self):
+        return len(self.value)
