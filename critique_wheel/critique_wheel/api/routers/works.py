0f47316c293f5f31189229f754b193017ac0263f
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-11-28 23:49:33 +0100

    Simplify for initial testing, create only

--- critique_wheel/critique_wheel/api/routers/works.py
+++ critique_wheel/critique_wheel/api/routers/works.py
@@ -3,27 +3,18 @@ import fastapi.exception_handlers
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from critique_wheel.adapters.sqlalchemy import work_repository
 from critique_wheel.api.schemas.work_schemas import (
     WorkPayloadSchema,
     WorkResponseSchema,
 )
-from critique_wheel.infrastructure.config import config as infr_config
-from critique_wheel.works.services.work_service import WorkService
+from critique_wheel.infrastructure.config import config
 
 router = fastapi.APIRouter()
 
 
 def get_db_session():
-    get_session = sessionmaker(bind=create_engine(infr_config.get_postgres_uri()))
+    get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
     return get_session()
-
-
-@router.get("/works")
-def list_works(session=fastapi.Depends(get_db_session)):
-    repo = work_repository.SqlAlchemyWorkRepository(session)
-    service = WorkService(repo)
-    return service.list_works()
 
 
 @router.post("/works", response_model=WorkResponseSchema, status_code=201)
