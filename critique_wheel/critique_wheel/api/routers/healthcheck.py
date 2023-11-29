13ad13bd690f93cf7245e63e5680c59f7893d04d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-11-29 14:00:16 +0100

    Refactor healthcheck for simplest version

--- critique_wheel/critique_wheel/api/routers/healthcheck.py
+++ critique_wheel/critique_wheel/api/routers/healthcheck.py
@@ -1,17 +1,8 @@
 import fastapi
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from critique_wheel.infrastructure.config import config as infr_config
 
 router = fastapi.APIRouter()
 
 
-def get_db_session():
-    get_session = sessionmaker(bind=create_engine(infr_config.get_postgres_uri()))
-    return get_session()
-
-
 @router.get("/healthcheck")
-async def healthcheck(session=fastapi.Depends(get_db_session)):
+async def healthcheck():
     return {"status": "ok"}
