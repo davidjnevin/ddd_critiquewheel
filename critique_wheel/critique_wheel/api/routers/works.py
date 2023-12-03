dafae907deedfc6fd5722a556deec8b33dc3f649
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-03 17:52:10 +0100

    Change import and use of get_postgres_uri to reflect new config location

--- critique_wheel/critique_wheel/api/routers/works.py
+++ critique_wheel/critique_wheel/api/routers/works.py
@@ -7,13 +7,13 @@ from critique_wheel.api.schemas.work_schemas import (
     WorkPayloadSchema,
     WorkResponseSchema,
 )
-from critique_wheel.infrastructure.config import config
+from critique_wheel.config import get_postgres_uri
 
 router = fastapi.APIRouter()
 
 
 def get_db_session():
-    get_session = sessionmaker(bind=create_engine(config.get_postgres_uri()))
+    get_session = sessionmaker(bind=create_engine(get_postgres_uri()))
     return get_session()
 
 
