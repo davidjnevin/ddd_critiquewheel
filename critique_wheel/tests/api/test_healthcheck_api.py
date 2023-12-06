d1a4e31af2670759e767466f08aac7c8301f24a4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-06 20:39:13 +0100

    Change to use AsyncClient

--- critique_wheel/tests/api/test_healthcheck_api.py
+++ critique_wheel/tests/api/test_healthcheck_api.py
@@ -1,13 +1,11 @@
 import pytest
-import requests
-
-from critique_wheel import config
+from httpx import AsyncClient
 
 
-@pytest.mark.usefixtures("restart_api")
-def test_healthcheck_api_returns_ok():
-    url = config.get_api_url()
-
-    response = requests.get(f"{url}/healthcheck")
+@pytest.mark.anyio
+async def test_healthcheck_api_returns_ok(
+    async_client: AsyncClient,
+):
+    response = await async_client.get("/healthcheck")
     assert response.status_code == 200
     assert response.json() == {"status": "ok"}
