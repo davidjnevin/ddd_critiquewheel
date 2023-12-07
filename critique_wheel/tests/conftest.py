a8081bd9e15da15a2c12c59ea6e6d92faf236203
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-07 12:08:30 +0100

    Remove restart_api fixture

--- critique_wheel/tests/conftest.py
+++ critique_wheel/tests/conftest.py
@@ -1,6 +1,5 @@
 import logging
 import time
-from pathlib import Path
 
 import pytest
 import requests
@@ -37,11 +36,11 @@ from critique_wheel.works.value_objects import (
 logger = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope="module")
-def restart_api():
-    (Path(__file__).parent.parent / "critique_wheel" / "main.py").touch()
-    time.sleep(0.5)  # hack to force reload in dev mode
-    wait_for_webapp_to_come_up()
+# @pytest.fixture(scope="module")
+# def restart_api():
+#     (Path(__file__).parent.parent / "critique_wheel" / "main.py").touch()
+#     time.sleep(0.5)  # hack to force reload in dev mode
+#     wait_for_webapp_to_come_up()
 
 
 @pytest.fixture
