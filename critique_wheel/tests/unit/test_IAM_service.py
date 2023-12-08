660b521e0c51b0267bd98950c8edb1c0748395a9
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-08 01:26:45 +0100

    Remove domain dependencies, using primitives in tests

--- critique_wheel/tests/unit/test_IAM_service.py
+++ critique_wheel/tests/unit/test_IAM_service.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from uuid import uuid4
 
 import pytest
@@ -18,20 +19,26 @@ class FakeSession:
         self.committed = True
 
 
-def test_create_member(member_details):
-    # Arrange
-    new_member = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-        member_type=member_details["member_type"],
-        status=member_details["status"],
+def create_member_for_testing(member_details, repo, session):
+    username = member_details["username"]
+    email = member_details["email"]
+    password = member_details["password"]
+
+    return iam_service.create_member(
+        username,
+        email,
+        password,
+        repo,
+        session,
     )
 
+
+def test_create_member(member_details):
+    # Arrange
     repo = FakeMemberRepository([])
     session = FakeSession()
+    result = create_member_for_testing(member_details, repo, session)
     # Act
-    result = iam_service.create_member(new_member, repo, session)
 
     # Assert
     assert result is not None
@@ -41,22 +48,16 @@ def test_create_member(member_details):
 
 def test_login_member_valid_credentials(member_details):
     # Arrange
-    new_member = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-        member_type=member_details["member_type"],
-        status=member_details["status"],
-    )
+    username = member_details["username"]
+    email = member_details["email"]
+    password = member_details["password"]
 
     repo = FakeMemberRepository([])
     session = FakeSession()
-    new_member_id = iam_service.create_member(new_member, repo, session)
+    new_member_id = iam_service.create_member(username, email, password, repo, session)
 
     # Act
-    returned_member = iam_service.login_member(
-        new_member.email, new_member.password, repo, session
-    )
+    returned_member = iam_service.login_member(email, password, repo, session)
 
     # Assert
     assert returned_member is not None
@@ -67,40 +68,32 @@ def test_login_member_valid_credentials(member_details):
 
 def test_login_member_raises_InvalidCredentials_with_invalid_password(member_details):
     # Arrange
-    new_member = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-        member_type=member_details["member_type"],
-        status=member_details["status"],
-    )
+    username = member_details["username"]
+    email = member_details["email"]
+    password = member_details["password"]
 
     repo = FakeMemberRepository([])
     session = FakeSession()
-    iam_service.create_member(new_member, repo, session)
+    iam_service.create_member(username, email, password, repo, session)
 
     # Assert
     with pytest.raises(iam_service.InvalidCredentials, match="Invalid credentials"):
-        iam_service.login_member(new_member.email, "incorrect_password", repo, session)
+        iam_service.login_member(email, "incorrect_password", repo, session)
 
 
 def test_login_member_raises_InvalidCredentials_for_nonexistent_email(member_details):
     # Arrange
-    new_member = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-        member_type=member_details["member_type"],
-        status=member_details["status"],
-    )
+    username = member_details["username"]
+    email = member_details["email"]
+    password = member_details["password"]
 
     repo = FakeMemberRepository([])
     session = FakeSession()
-    iam_service.create_member(new_member, repo, session)
+    iam_service.create_member(username, email, password, repo, session)
     non_existant_email = "this_email_does_not_exist@davidnevin.net"
     # Assert
     with pytest.raises(iam_service.InvalidCredentials, match="Invalid credentials"):
-        iam_service.login_member(non_existant_email, new_member.password, repo, session)
+        iam_service.login_member(non_existant_email, password, repo, session)
 
 
 def test_register_new_member_with_valid_registration_creates_new_member(member_details):
@@ -265,22 +258,16 @@ def test_new_member_with_existing_username_raises_DuplicateEntryError(member_det
 
 def test_list_members(member_details):
     # Arrange
-    new_member_1 = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-    )
-
     repo = FakeMemberRepository([])
     session = FakeSession()
-    new_member_1.id = iam_service.create_member(new_member_1, repo, session)
+    new_member_1 = create_member_for_testing(member_details, repo, session)
 
-    new_member_2 = Member(
-        username="other_username",
-        email="other_email@davidnevin.net",
-        password="other_p@ssword",
-    )
-    new_member_2.id = iam_service.create_member(new_member_2, repo, session)
+    another_member_details = {
+        "username": "other_username",
+        "email": "other_email@davidnevin.net",
+        "password": "other_p@ssword",
+    }
+    new_member_2 = create_member_for_testing(another_member_details, repo, session)
 
     # Act
     members_list = iam_service.list_members(repo, session)
@@ -303,59 +290,43 @@ def test_list_members_returns_empty_list_when_no_members():
 
 def test_get_member_by_id_returns_member_with_matching_id(member_details):
     # Arrange
-    new_member_1 = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-    )
-
     repo = FakeMemberRepository([])
     session = FakeSession()
-    new_member_1_id = iam_service.create_member(new_member_1, repo, session)
+    new_member_1 = create_member_for_testing(member_details, repo, session)
 
     # Act
-    member = iam_service.get_member_by_id(new_member_1_id, repo, session)
+    member = iam_service.get_member_by_id(str(new_member_1.id), repo)
 
     # Assert
     assert member is not None
-    assert member.id == new_member_1_id
+    assert str(member.id) == str(new_member_1.id)
     assert str(member.username) == member_details["username"]
 
 
 def test_get_member_by_id_returns_None_for_nonexistent_id():
     repo = FakeMemberRepository([])
-    session = FakeSession()
     nonexistent_id = str(uuid4())
-    assert iam_service.get_member_by_id(nonexistent_id, repo, session) is None
+    assert iam_service.get_member_by_id(nonexistent_id, repo) is None
 
 
 def test_get_member_by_username_returns_member_with_matching_username(member_details):
     # Arrange
-    new_member_1 = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-    )
-
     repo = FakeMemberRepository([])
     session = FakeSession()
-    iam_service.create_member(new_member_1, repo, session)
+    create_member_for_testing(member_details, repo, session)
 
     # Act
-    member = iam_service.get_member_by_username(new_member_1.username, repo, session)
+    member = iam_service.get_member_by_username(member_details["username"], repo)
 
     # Assert
     assert member is not None
-    assert member.username == new_member_1.username
+    assert str(member.username) == member_details["username"]
 
 
 def test_get_member_by_username_returns_None_for_nonexistent_username():
     repo = FakeMemberRepository([])
-    session = FakeSession()
     nonexistent_username = "nonexistent_username"
-    assert (
-        iam_service.get_member_by_username(nonexistent_username, repo, session) is None
-    )
+    assert iam_service.get_member_by_username(nonexistent_username, repo) is None
 
 
 def test_add_work_to_member_adds_work_to_member(member_details, work_details):
@@ -363,13 +334,8 @@ def test_add_work_to_member_adds_work_to_member(member_details, work_details):
     repo = FakeMemberRepository([])
     work_repo = FakeWorkRepository([])
     session = FakeSession()
-    new_member_1 = Member(
-        username=member_details["username"],
-        email=member_details["email"],
-        password=member_details["password"],
-    )
 
-    new_member_1_id = iam_service.create_member(new_member_1, repo, session)
+    new_member_1_id = create_member_for_testing(member_details, repo, session)
 
     title = work_details["title"]
     content = work_details["content"]
@@ -381,19 +347,77 @@ def test_add_work_to_member_adds_work_to_member(member_details, work_details):
         content=content,
         genre=genre,
         age_restriction=age_restriction,
-        member_id=new_member_1_id,
+        member_id=str(new_member_1_id),
         repo=work_repo,
         session=session,
     )
 
     # Act
     iam_service.add_work_to_member(
-        member_id=new_member_1_id, work=new_work, repo=repo, session=session
+        member_id=str(new_member_1_id),
+        work_id=str(new_work.id),
+        repo=repo,
+        work_repo=work_repo,
+        session=session,
     )
-    new_member: MemberId() = iam_service.get_member_by_id(
-        new_member_1_id, repo, session
+    new_member: Optional[Member] = iam_service.get_member_by_id(
+        str(new_member_1_id), repo
     )
 
     # Assert
     assert len(new_member.works) == 1
     assert new_work in new_member.works
+
+
+def test_add_work_to_member_with_no_work_raises_InvalidEntryError(
+    member_details, work_details
+):
+    # Arrange
+    repo = FakeMemberRepository([])
+    work_repo = FakeWorkRepository([])
+    session = FakeSession()
+
+    new_member_1_id = create_member_for_testing(member_details, repo, session)
+
+    # Act
+    with pytest.raises(iam_service.InvalidEntryError):
+        iam_service.add_work_to_member(
+            member_id=str(new_member_1_id),
+            work_id="",
+            repo=repo,
+            work_repo=work_repo,
+            session=session,
+        )
+
+    def test_add_work_to_member_with_no_member_raises_MemberNotFoundException(
+        member_details, work_details
+    ):
+        # Arrange
+        repo = FakeMemberRepository([])
+        work_repo = FakeWorkRepository([])
+        session = FakeSession()
+
+        title = work_details["title"]
+        content = work_details["content"]
+        genre = work_details["genre"]
+        age_restriction = work_details["age_restriction"]
+
+        new_work = work_service.add_work(
+            title=title,
+            content=content,
+            genre=genre,
+            age_restriction=age_restriction,
+            member_id=str(uuid4()),
+            repo=work_repo,
+            session=session,
+        )
+
+        # Act
+        with pytest.raises(iam_service.MemberNotFoundException):
+            iam_service.add_work_to_member(
+                member_id="",
+                work_id=str(new_work.id),
+                repo=repo,
+                work_repo=work_repo,
+                session=session,
+            )
