430582aad7b39c0d3cc5c6f3acaf8fb62ed3aaf7
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-09 18:37:56 +0100

    Change to test for int(Rating(5))

--- critique_wheel/tests/unit/test_orm.py
+++ critique_wheel/tests/unit/test_orm.py
@@ -119,7 +119,7 @@ def test_create_and_retreive_rating(session, valid_critique, valid_rating):
     # Assert
     assert retrieved_rating.id == new_rating.id
     assert retrieved_rating.member_id == new_rating.member_id
-    assert retrieved_rating.score == RatingScore(5)
+    assert retrieved_rating.score == int(RatingScore(5))
 
 
 # @pytest.mark.skip(reason="Throwaway test file for testing ORM functionality")
