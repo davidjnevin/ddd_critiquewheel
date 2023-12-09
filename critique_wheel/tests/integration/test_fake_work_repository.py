c16aba0e70ceea892898e9bceb298ed12f3936ad
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-09 18:38:53 +0100

    Refactor to ensure unique valid_work and valid_member id's

--- critique_wheel/tests/integration/test_fake_work_repository.py
+++ critique_wheel/tests/integration/test_fake_work_repository.py
@@ -1,6 +1,10 @@
 from uuid import uuid4
 
+import pytest
+
 from critique_wheel.members.models.IAM import MemberStatus
+from critique_wheel.members.value_objects import MemberId
+from critique_wheel.works.value_objects import WorkId
 from tests.integration import fake_work_repository
 
 
@@ -22,27 +26,36 @@ def test_repository_can_save_a_work(
     assert repo.get_work_by_id(work_2.id) == work_2
 
 
-def test_repository_can_get_a_work_by_id(session, valid_work):
+@pytest.mark.current
+def test_repository_can_get_a_work_by_id(session, valid_work, active_valid_member):
     work = valid_work
     repo = fake_work_repository.FakeWorkRepository(session)
+    valid_work.id = WorkId.from_string(str(uuid4()))
+    work.member_id = active_valid_member.id
     repo.add(work)
     session.commit()
     assert repo.get_work_by_id(work.id) == work
-    assert repo.list() == [work]
+    assert work in repo.list()
 
 
-def test_repository_can_get_work_by_member_id(session, valid_work):
+@pytest.mark.current
+def test_repository_can_get_work_by_member_id(session, valid_work, active_valid_member):
     work = valid_work
+    active_valid_member.id = MemberId.from_string(str(uuid4()))
+    valid_work.id = WorkId.from_string(str(uuid4()))
+    work.member_id = active_valid_member.id
     repo = fake_work_repository.FakeWorkRepository(session)
     repo.add(work)
     session.commit()
 
     assert repo.get_work_by_member_id(work.member_id) == work
-    assert repo.list() == [work]
+    assert work in repo.list()
 
 
+@pytest.mark.current
 def test_respository_returns_none_when_no_work_found(session):
     repo = fake_work_repository.FakeWorkRepository(session)
-    id = uuid4()
-    assert repo.get_work_by_id(id) is None
-    assert repo.get_work_by_member_id(id) is None
+    work_id = WorkId.from_string(str(uuid4()))
+    member_id = MemberId.from_string(str(uuid4()))
+    assert repo.get_work_by_id(work_id) is None
+    assert repo.get_work_by_member_id(member_id) is None
