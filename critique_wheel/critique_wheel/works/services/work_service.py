4bdc476486328bbaea996f87794db75a247d8f73
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-10 18:56:33 +0100

    Implement Unit of Work versus repo-session

--- critique_wheel/critique_wheel/works/services/work_service.py
+++ critique_wheel/critique_wheel/works/services/work_service.py
@@ -1,11 +1,11 @@
 import logging
 from typing import Optional
 
-from critique_wheel.members.value_objects import MemberId
+from critique_wheel.members import value_objects as member_value_objects
+from critique_wheel.works import value_objects
 from critique_wheel.works.exceptions import exceptions
-from critique_wheel.works.models.work import Work
-from critique_wheel.works.models.work_repository import AbstractWorkRepository
-from critique_wheel.works.value_objects import Content, Title, WorkId
+from critique_wheel.works.models import work as model
+from critique_wheel.works.services import unit_of_work as uow
 
 logger = logging.getLogger(__name__)
 
@@ -27,44 +27,55 @@ class InvalidDataError(BaseWorkServiceError):
 
 
 def add_work(
-    repo: AbstractWorkRepository,
-    session,
+    uow: uow.AbstractUnitOfWork,
     title: str,
     content: str,
     member_id: str,
-    genre,
-    age_restriction,
-    work_id: str = "",
-    critiques=None,
+    genre: str,
+    age_restriction: str,
+    work_id: Optional[str] = "",
+    critiques: Optional[list] = None,
 ) -> dict:
-    try:
-        new_work = Work.create(
-            work_id=WorkId.from_string(uuid_string=work_id) or WorkId(),
-            title=Title(title),
-            content=Content(content),
-            member_id=MemberId.from_string(uuid_string=member_id),
-            age_restriction=age_restriction,
-            genre=genre,
-            critiques=critiques,
-        )
-    except exceptions.MissingEntryError as e:
-        logger.exception(f"Missing entry encountered: {e}")
-        raise InvalidDataError(f"Invalid data encountered: {e}") from e
-    except Exception as e:
-        logger.exception(f"Invalid data encountered: {e}")
-        raise InvalidDataError(f"Invalid data encountered: {e}") from e
-    if work_id:
-        if repo.get_work_by_id(new_work.id):
+    with uow:
+        try:
+            new_work = model.Work.create(
+                work_id=value_objects.WorkId.from_string(uuid_string=work_id)
+                or value_objects.WorkId(),
+                title=value_objects.Title(title),
+                content=value_objects.Content(content),
+                member_id=member_value_objects.MemberId.from_string(
+                    uuid_string=member_id
+                ),
+                age_restriction=value_objects.find_agerestriction_by_value(
+                    age_restriction
+                ),
+                genre=value_objects.find_genre_by_value(genre),
+                critiques=critiques,
+            )
+        except exceptions.MissingEntryError as e:
+            logger.exception(f"Missing entry encountered: {e}")
+            raise InvalidDataError(f"Invalid data encountered: {e}") from e
+        except Exception as e:
+            logger.exception(f"Invalid data encountered: {e}")
+            raise InvalidDataError(f"Invalid data encountered: {e}") from e
+        if uow.works.get_work_by_id(new_work.id):
             raise DuplicateWorkError(f"Work with id {str(new_work.id)} already exists")
-    repo.add(new_work)
-    session.commit()
+        uow.works.add(new_work)
+        uow.commit()
     return new_work.to_dict()
 
 
-def list_works(repo) -> list[Work]:
-    return [work.to_dict() for work in repo.list()]
+def list_works(
+    uow: uow.AbstractUnitOfWork,
+) -> list[dict]:
+    with uow:
+        works = [work.to_dict() for work in uow.works.list()]
+    return works
 
 
-def get_work_by_id(work_id: str, repo: AbstractWorkRepository) -> Optional[dict]:
-    work = repo.get_work_by_id(WorkId.from_string(uuid_string=work_id))
+def get_work_by_id(work_id: str, uow: uow.AbstractUnitOfWork) -> Optional[dict]:
+    with uow:
+        work = uow.works.get_work_by_id(
+            value_objects.WorkId.from_string(uuid_string=work_id)
+        )
     return work.to_dict() if work else None
