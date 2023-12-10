b047322896582e41717f4b1222dbd087620bbeda
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-10 18:55:45 +0100

    Rename SqlAlchemyUnitOFWork to WorkUnitOfWork

--- critique_wheel/critique_wheel/works/services/unit_of_work.py
+++ critique_wheel/critique_wheel/works/services/unit_of_work.py
@@ -35,7 +35,7 @@ DEFAULT_SESSION_FACTORY = sessionmaker(
 )
 
 
-class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+class WorkUnitOfWork(AbstractUnitOfWork):
     def __init__(self, session_factory=DEFAULT_SESSION_FACTORY):
         self.session_factory = session_factory
 
