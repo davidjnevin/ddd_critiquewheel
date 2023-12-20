1ed772bfbaff07635bebddc61bd1a8f11139a33d
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-20 12:45:53 +0100

    Add get_session_local and get_postgres_uri to database helper

---
+++ critique_wheel/critique_wheel/infrastructure/database.py
@@ -0,0 +1,26 @@
+import logging
+
+import sqlalchemy
+import sqlalchemy.ext.declarative
+import sqlalchemy.orm
+
+from critique_wheel import config
+
+logger = logging.getLogger(__name__)
+
+
+def get_engine():
+    logger.debug("Creating database engine...")
+    engine = sqlalchemy.create_engine(
+        config.get_postgres_uri(),
+        # connect_args={"check_same_thread": False},
+    )
+    return engine
+
+
+def get_session_local():
+    logger.debug("Creating database session...")
+    SessionLocal = sqlalchemy.orm.sessionmaker(
+        autocommit=False, autoflush=False, bind=get_engine()
+    )
+    return SessionLocal
