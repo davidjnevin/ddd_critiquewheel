8a5f075f1ebd629b6a35ff5f1f28182c549a9f38
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-31 18:36:28 +0100

    Simplify test_client

--- critique_wheel/tests/e2e/test_member_endpoints.py
+++ critique_wheel/tests/e2e/test_member_endpoints.py
@@ -3,16 +3,12 @@ import uuid
 
 import pytest
 from fastapi.testclient import TestClient
-from httpx import AsyncClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from critique_wheel import config
-from critique_wheel.adapters.sqlalchemy import iam_repository
 from critique_wheel.entrypoints.routers.works import get_db_session
-from critique_wheel.infrastructure import database as db_config
 from critique_wheel.main import app
-from critique_wheel.members.services import iam_service
 
 logger = logging.getLogger(__name__)
 
@@ -23,22 +19,11 @@ def override_get_local_db_session():
 
 
 app.dependency_overrides[get_db_session] = override_get_local_db_session
-
-
-@pytest.fixture
-def test_client():
-    with TestClient(app) as client:
-        client.base_url = config.get_api_url()
-        client.headers["Content-Type"] = "application/json"
-        client.headers["Accept"] = "application/json"
-        yield client
+test_client = TestClient(app)
 
 
 @pytest.mark.current
-@pytest.mark.anyio
-async def test_create_member_endpoint_returns_work(
-    test_client: TestClient,
-):
+def test_create_member_endpoint_returns_member():
     payload = {
         "username": "PeterPan",
         "email": "some_other_random@email.com",
@@ -55,38 +40,7 @@ async def test_create_member_endpoint_returns_work(
     assert response.json()["password"] != payload["password"]
 
 
-# @pytest.mark.current
-@pytest.mark.anyio
-# @pytest.mark.usefixtures("postgres_db")
-async def test_work_api_returns_work(
-    async_client: AsyncClient,
-    work_details,
-):
-    session = db_config.get_session_local()()
-    repo = iam_repository.MemberRepository(session)
-    member_id = iam_service.add_member(
-        username="PeterPan",
-        email="some_random@email.com",
-        password="wertsdfsa12D!",
-        session=session,
-        repo=repo,
-    )
-
-    work_details["member_id"] = str(member_id)
-    logger.debug(work_details)
-    add_response = await async_client.post("/work", json=work_details)
-    breakpoint()
-    logger.debug(add_response.json())
-    work_id = add_response.json()["id"]
-    response = await async_client.get(f"/work/{work_id}")
-    assert response.status_code == 200
-    assert response.json()["title"] == work_details["title"]
-
-
-@pytest.mark.anyio
-async def test_work_api_returns_404_if_id_does_not_exist(
-    async_client: AsyncClient,
-):
-    nonexistant_work_id = uuid.uuid4()
-    response = await async_client.get(f"/work/{nonexistant_work_id}")
+def test_member_endpoint_returns_404_if_id_does_not_exist():
+    nonexistant_member_id = uuid.uuid4()
+    response = test_client.get(f"/work/{nonexistant_member_id}")
     assert response.status_code == 404
