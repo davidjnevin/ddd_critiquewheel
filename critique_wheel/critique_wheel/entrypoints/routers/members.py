75d41cd53665fb8c9209249f9ace8e580d7a8bac
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-31 14:02:32 +0100

    Add unit of work

--- critique_wheel/critique_wheel/entrypoints/routers/members.py
+++ critique_wheel/critique_wheel/entrypoints/routers/members.py
@@ -4,32 +4,33 @@ import fastapi
 import fastapi.exception_handlers
 from sqlalchemy.orm import Session
 
-from critique_wheel.api.schemas import schemas
+from critique_wheel.entrypoints.schemas import schemas
 from critique_wheel.infrastructure import database as db_config
-from critique_wheel.members.services import member_service, unit_of_work
+from critique_wheel.members.services import iam_service, unit_of_work
 
 logger = logging.getLogger(__name__)
 
 router = fastapi.APIRouter()
 
 
-async def get_db_session():
+def get_db_session():
     db = db_config.get_session_local()
     try:
         yield db
     finally:
-        db.close
+        # db().execute.text("DELETE from member")
+        db.close()
 
 
-@router.post("/member")
-async def create_member(
+@router.post("/member", response_model=schemas.UserMember)
+def create_member(
     member: schemas.UserMemberIn,
     session_factory: Session = fastapi.Depends(get_db_session),
 ):
     logger.debug("Creating member.")
-    result = member_service.add_member(
-        uow=unit_of_work.MemberUnitOfWork(session_factory),
-        username=member.title,
+    result = iam_service.add_member(
+        uow=unit_of_work.IAMUnitOfWork(session_factory),
+        username=member.username,
         email=member.email,
         password=member.password,
     )
