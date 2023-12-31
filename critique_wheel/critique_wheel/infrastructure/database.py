1cc45f321251ea4050001f248d47539bc58331b4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-31 19:44:13 +0100

    Move get_postres_uri to this module

--- critique_wheel/critique_wheel/infrastructure/database.py
+++ critique_wheel/critique_wheel/infrastructure/database.py
@@ -9,14 +9,25 @@ from critique_wheel import config
 logger = logging.getLogger(__name__)
 
 
+# DB configuration
+def get_postgres_uri():
+    logger.debug(f"ENV_STATE is {config.BaseConfig().ENV_STATE}")
+    host = config.config.DB_HOST
+    port = 54321 if host == "localhost" else 5432
+    password = config.config.DB_PASSWORD
+    user = config.config.DB_USER
+    db_name = config.config.DB_NAME
+    logger.debug(f"Using {db_name}")
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
 def get_engine():
     logger.debug("Creating database engine...")
-    logger.debug(f"Using {config.BaseConfig().ENV_STATE} state")
     engine = sqlalchemy.create_engine(
-        config.get_postgres_uri(),
+        get_postgres_uri(),
         # connect_args={"check_same_thread": False},
     )
-    logger.debug(f"Database {config.get_postgres_uri()} created.")
+    logger.debug(f"Database {get_postgres_uri()} created.")
     return engine
 
 
