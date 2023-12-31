4a3513bfe3233a90cf5ea6448bc0a532c19bc530
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-12-31 18:35:55 +0100

    Add ENV_STATE logging

--- critique_wheel/critique_wheel/infrastructure/database.py
+++ critique_wheel/critique_wheel/infrastructure/database.py
@@ -10,15 +10,18 @@ logger = logging.getLogger(__name__)
 
 
 def get_engine():
+    logger.debug(f"Using {config.BaseConfig().ENV_STATE}")
     logger.debug("Creating database engine...")
     engine = sqlalchemy.create_engine(
         config.get_postgres_uri(),
         # connect_args={"check_same_thread": False},
     )
+    logger.debug(f"Database {config.get_postgres_uri()} created.")
     return engine
 
 
 def get_session_local():
+    logger.debug(f"Using {config.BaseConfig().ENV_STATE}")
     logger.debug("Creating database session...")
     SessionLocal = sqlalchemy.orm.sessionmaker(
         autocommit=False, autoflush=False, bind=get_engine()
