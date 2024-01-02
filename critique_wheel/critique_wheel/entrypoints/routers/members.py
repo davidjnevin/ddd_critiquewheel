efe17199831f9459da251ccde30ef3227dd1d987
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-02 19:23:28 +0100

    Simplify get_db_session and remove dependency injection in endpoint

--- critique_wheel/critique_wheel/entrypoints/routers/members.py
+++ critique_wheel/critique_wheel/entrypoints/routers/members.py
@@ -2,7 +2,6 @@ import logging
 
 import fastapi
 import fastapi.exception_handlers
-from sqlalchemy.orm import Session
 
 from critique_wheel.entrypoints.schemas import schemas
 from critique_wheel.infrastructure import database as db_config
@@ -14,17 +13,14 @@ router = fastapi.APIRouter()
 
 
 def get_db_session():
-    db = db_config.get_session_local()()
-    try:
-        yield db
-    finally:
-        db.close()
+    db = db_config.get_session_local()
+    yield db
 
 
 @router.post("/member", response_model=schemas.UserMember)
 def create_member(
     member: schemas.UserMemberIn,
-    session_factory: Session = fastapi.Depends(get_db_session),
+    #     db: Session = fastapi.Depends(get_db_session),
 ):
     logger.debug("Creating member.")
     result = iam_service.add_member(
