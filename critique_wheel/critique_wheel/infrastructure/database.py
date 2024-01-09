07eee8855f0ea5b61eee0725b1ae3e032a468e0b
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-09 18:12:38 +0100

    Clarify logging message

--- critique_wheel/critique_wheel/infrastructure/database.py
+++ critique_wheel/critique_wheel/infrastructure/database.py
@@ -5,6 +5,7 @@ import sqlalchemy.ext.declarative
 import sqlalchemy.orm
 
 from critique_wheel import config
+from critique_wheel.adapters.orm import mapper_registry
 
 logger = logging.getLogger(__name__)
 
@@ -17,25 +18,27 @@ def get_postgres_uri():
     password = config.config.DB_PASSWORD
     user = config.config.DB_USER
     db_name = config.config.DB_NAME
-    logger.debug(f"Using {db_name}")
+    logger.debug(f"DB_NAME is {db_name}")
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
 
 
 def get_engine():
+    logger.debug(f"Using {config.BaseConfig().ENV_STATE} state")
     logger.debug("Creating database engine...")
     engine = sqlalchemy.create_engine(
         get_postgres_uri(),
         # connect_args={"check_same_thread": False},
     )
+    mapper_registry.metadata.create_all(engine)
     logger.debug(f"Database {get_postgres_uri()} created.")
     return engine
 
 
 def get_session_local():
-    logger.debug("Creating database session...")
     logger.debug(f"Using {config.BaseConfig().ENV_STATE} state")
-    SessionLocal = sqlalchemy.orm.sessionmaker(
+    logger.debug("Creating database session...")
+    LocalSessionMaker = sqlalchemy.orm.sessionmaker(
         autocommit=False, autoflush=False, bind=get_engine()
     )
 
-    return SessionLocal
+    return LocalSessionMaker
