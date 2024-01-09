b10414310a189d002e15b9a73ec14bb528e97e0c
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-09 18:14:23 +0100

    Add injection dependency to member endpoint

--- critique_wheel/critique_wheel/entrypoints/routers/members.py
+++ critique_wheel/critique_wheel/entrypoints/routers/members.py
@@ -2,6 +2,7 @@ import logging
 
 import fastapi
 import fastapi.exception_handlers
+from sqlalchemy.orm import Session
 
 from critique_wheel.entrypoints.schemas import schemas
 from critique_wheel.infrastructure import database as db_config
@@ -13,18 +14,17 @@ router = fastapi.APIRouter()
 
 
 def get_db_session():
-    db = db_config.get_session_local()
+    db: Session = db_config.get_session_local()
     yield db
 
 
 @router.post("/member", response_model=schemas.UserMember)
 def create_member(
-    member: schemas.UserMemberIn,
-    #     db: Session = fastapi.Depends(get_db_session),
+    member: schemas.UserMemberIn, db: Session = fastapi.Depends(get_db_session)
 ):
     logger.debug("Creating member.")
     result = iam_service.add_member(
-        uow=unit_of_work.IAMUnitOfWork(),
+        uow=unit_of_work.IAMUnitOfWork(db),
         username=member.username,
         email=member.email,
         password=member.password,
