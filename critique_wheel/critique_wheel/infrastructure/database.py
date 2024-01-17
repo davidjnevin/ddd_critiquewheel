d8d42b6b707dd41f852f3b2483ecc1305008c197
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-17 23:08:34 +0100

    Add sql specific local sessions and engine

--- critique_wheel/critique_wheel/infrastructure/database.py
+++ critique_wheel/critique_wheel/infrastructure/database.py
@@ -10,7 +10,7 @@ from critique_wheel.adapters.orm import mapper_registry
 logger = logging.getLogger(__name__)
 
 
-# DB configuration
+# Postgres DB configuration
 def get_postgres_uri():
     logger.debug(f"ENV_STATE is {config.BaseConfig().ENV_STATE}")
     host = config.config.DB_HOST
@@ -20,6 +20,13 @@ def get_postgres_uri():
     db_name = config.config.DB_NAME
     logger.debug(f"DB_NAME is {db_name}")
     return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+# Sqlite DB configuration
+def get_sqlite_uri():
+    logger.debug(f"ENV_STATE is {config.BaseConfig().ENV_STATE}")
+    logger.debug("Using sqlite in memory database")
+    return "sqlite:///:memory:"
 
 
 def get_engine():
@@ -34,11 +41,38 @@ def get_engine():
     return engine
 
 
+def get_in_memory_engine():
+    logger.debug(f"Using {config.BaseConfig().ENV_STATE} state")
+    logger.debug("Creating database engine...")
+    engine = sqlalchemy.create_engine(
+        get_sqlite_uri(),
+        connect_args={"check_same_thread": False},
+        echo=True,
+    )
+    mapper_registry.metadata.create_all(engine)
+    logger.debug(f"Engine {engine} created.")
+    return engine
+
+
+def get_in_memory_session_local():
+    logger.debug(f"Using {config.BaseConfig().ENV_STATE} state")
+    logger.debug("Creating database session...")
+    LocalSessionMaker = sqlalchemy.orm.sessionmaker(
+        # autocommit=False,
+        # autoflush=False,
+        bind=get_in_memory_engine(),
+    )
+
+    return LocalSessionMaker
+
+
 def get_session_local():
     logger.debug(f"Using {config.BaseConfig().ENV_STATE} state")
     logger.debug("Creating database session...")
     LocalSessionMaker = sqlalchemy.orm.sessionmaker(
-        autocommit=False, autoflush=False, bind=get_engine()
+        autocommit=False,
+        autoflush=False,
+        bind=get_engine(),
     )
 
     return LocalSessionMaker
