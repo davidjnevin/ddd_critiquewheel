f65f68e37eca4714fe0a265b052fede1f23a21be
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-18 20:20:48 +0100

    Add sqlite engine, session definition to test

--- critique_wheel/tests/e2e/test_member_endpoints.py
+++ critique_wheel/tests/e2e/test_member_endpoints.py
@@ -1,39 +1,48 @@
 import logging
 import uuid
 
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
-from critique_wheel.adapters import orm
-from critique_wheel.entrypoints.routers.works import get_db_session
-from critique_wheel.infrastructure import database as db_config
+from critique_wheel.adapters.orm import mapper_registry
+from critique_wheel.entrypoints.routers import members as members_router
 from critique_wheel.main import app
 
 logger = logging.getLogger(__name__)
 
-engine = create_engine(db_config.get_sqlite_uri())
-logger.debug(f"Using {engine} database engine")
+pytestmark = pytest.mark.usefixtures("mappers")
+SQLITE_DB_URI = "sqlite:///"
 
 
-def override_get_local_db_session():
+def override_get_db_session():
+    engine = create_engine(
+        SQLITE_DB_URI,
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+        echo=True,
+    )
+    logger.debug(f"Using {engine} database engine")
+    mapper_registry.metadata.create_all(engine)
     try:
-        orm.start_mappers()
-        local_db_session = sessionmaker(
+        db = sessionmaker(
+            autocommit=False,
+            autoflush=False,
             bind=engine,
-            # autocommit=False,
-            # autoflush=False,
         )
-        db = local_db_session
         yield db
     finally:
-        db.close()
+        db().close()
 
 
-app.dependency_overrides[get_db_session] = override_get_local_db_session
+app.include_router(members_router.router)
 test_client = TestClient(app)
+app.dependency_overrides[members_router.get_db_session] = override_get_db_session
 
 
+@pytest.mark.current
 def test_create_member_endpoint_returns_member():
     payload = {
         "username": "PeterPan",
@@ -42,12 +51,13 @@ def test_create_member_endpoint_returns_member():
         "status": "active",
     }
 
-    response = test_client.post("/member", json=payload)
+    response = test_client.post("/members/", json=payload)
     logger.debug(response.json())
     assert response.status_code == 201
 
 
+@pytest.mark.current
 def test_member_endpoint_returns_404_if_id_does_not_exist():
     nonexistant_member_id = uuid.uuid4()
-    response = test_client.get(f"/work/{nonexistant_member_id}")
+    response = test_client.get(f"/members/{nonexistant_member_id}")
     assert response.status_code == 404
