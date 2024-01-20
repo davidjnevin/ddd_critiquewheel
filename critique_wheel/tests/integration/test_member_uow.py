eec4e985d0c2ecf07d9fbad9ecc97861094ea299
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-20 14:43:52 +0100

    Add tests for automatic rollback in case of error or exception

--- critique_wheel/tests/integration/test_member_uow.py
+++ critique_wheel/tests/integration/test_member_uow.py
@@ -64,3 +64,59 @@ def test_uow_can_retrieve_a_member_and_alter_it(sqlite_session_factory):
 
     member = get_member_by_id(session, id)
     assert member.username == "new_username"
+
+
+@pytest.mark.current
+def test_roll_back_uncommited_changes_by_default(sqlite_session_factory):
+    session = sqlite_session_factory()
+    session.expire_on_commit = False
+    id = str(uuid4())
+    insert_member(
+        session,
+        id=id,
+        username="test_username",
+        password="secure_unguessab1e_p@ssword",
+        email="some_random@davidnevin.net",
+        member_type=MemberRole.MEMBER,
+        status=MemberStatus.ACTIVE,
+        works=None,
+        critiques=None,
+    )
+    session.commit()
+
+    uow = IAMUnitOfWork(sqlite_session_factory)
+    with uow:
+        member = uow.members.get_member_by_username("test_username")
+        member.username = "new_username"
+
+    member = get_member_by_id(session, id)
+    assert member.username == "test_username"
+
+
+@pytest.mark.current
+def test_rolls_back_on_errors(sqlite_session_factory):
+    session = sqlite_session_factory()
+    session.expire_on_commit = False
+    id = str(uuid4())
+    insert_member(
+        session,
+        id=id,
+        username="test_username",
+        password="secure_unguessab1e_p@ssword",
+        email="some_random@davidnevin.net",
+        member_type=MemberRole.MEMBER,
+        status=MemberStatus.ACTIVE,
+        works=None,
+        critiques=None,
+    )
+    session.commit()
+
+    uow = IAMUnitOfWork(sqlite_session_factory)
+    with pytest.raises(ValueError):
+        with uow:
+            member = uow.members.get_member_by_username("test_username")
+            member.username = "new_username"
+            raise ValueError()
+
+    member = get_member_by_id(session, id)
+    assert member.username == "test_username"
