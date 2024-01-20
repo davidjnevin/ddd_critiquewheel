b1b5489544ae0e507c6b8276399ebdf32f629ab4
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-01-20 14:51:12 +0100

    Move insert_member and get_member_by_id to helpers.py

--- critique_wheel/tests/integration/test_member_uow.py
+++ critique_wheel/tests/integration/test_member_uow.py
@@ -9,36 +9,6 @@ from tests.helpers import get_member_by_id, insert_member
 pytestmark = pytest.mark.usefixtures("mappers")
 
 
-# def insert_member(session, **kwargs):
-#     stmt = sqlalchemy.text(
-#         """
-#         INSERT INTO members (id, username, email, password)
-#         VALUES (:id, :username, :email, :password)
-#         """
-#     )
-#     params = kwargs
-#     session.execute(stmt, params)
-
-
-# def get_member_by_id(session, member_id):
-#     stmt = sqlalchemy.text(
-#         """
-#         SELECT id, username, email, password
-#         FROM members
-#         WHERE id = :id
-#         """
-#     )
-#     params = {
-#         "id": member_id,
-#     }
-#     result = session.execute(stmt, params)
-#     row = result.fetchone()
-#     if row:
-#         return row._mapping
-#     else:
-#         return None
-
-
 def test_uow_can_retrieve_a_member_and_alter_it(sqlite_session_factory):
     session = sqlite_session_factory()
     session.expire_on_commit = False
